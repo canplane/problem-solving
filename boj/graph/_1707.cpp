5f0bffa71c29703c787fdd83193b3cefe67ed459
canplane <canplane@gmail.com>
2021-02-10 23:56:12 +0900

    210210

--- boj/graph/_1707.cpp
+++ boj/graph/_1707.cpp
@@ -4,9 +4,20 @@
 #define MAX_VERTICES 20000
 
 vector<int> edges[MAX_VERTICES + 1];
-
 short int visited[MAX_VERTICES + 1];
 
+bool dfs(int v, int color, int N) {
+    visited[v] = color;
+    for (auto w = edges[v].begin(); w != edges[v].end(); w++) {
+        if (visited[w] == color)
+            return false;
+        else if (!visited[w] && !dfs(v, -color, N))
+            return false;
+    }
+    return true;
+}
+
+/*
 bool bfs(int v, int color, int N) {
     std::queue<int> q;
     q.push(v), visited[v] = color;
@@ -23,7 +34,7 @@ bool bfs(int v, int color, int N) {
         color *= -1;
     }
     return true;
-}
+}*/
 
 int main() {
     int K, V, E, i, j;
