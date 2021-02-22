1935bc02c9d7d738889ae60debf5ef766a2e8da5
canplane <canplane@gmail.com>
2021-02-22 22:47:41 +0900

    210222

--- boj/6_divide_conquer/_2261_2_divide_conquer.cpp
+++ boj/6_divide_conquer/_2261_2_divide_conquer.cpp
@@ -2,18 +2,19 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 vector<pair<int, int>> v;
 
 struct cmp_y {
     bool operator()(const pair<int, int>& p1, const pair<int, int>& p2) const {
-        return (p1.second != p2.second) ? (p1.second < p2.second) : (p1.first < p2.first);
-    }
+        return (p1.second != p2.second) ? p1.second < p2.second : p1.first < p2.first;
+    }  
 };
 
 int dist(pair<int, int> p1, pair<int, int> p2) {
-    return (p2.first - p1.first)*(p2.first - p1.first) + (p2.second - p1.second)*(p2.second - p1.second);
+    return pow(p2.first - p1.first, 2) + pow(p2.second - p1.second, 2);
 }
 
 int dc(int l, int r) {
@@ -21,27 +22,25 @@ int dc(int l, int r) {
         return 0x7fffffff;
     if (l + 1 == r)
         return dist(v[l], v[r]);
-    
     int mid = (l + r) / 2;
     int min_d = min(dc(l, mid), dc(mid + 1, r));
     
     vector<pair<int, int>> w;
     w.reserve(r - l + 1);
     for (int i = mid; i >= l; i--) {
-        if (dist({ v[i].first, 0 }, { v[mid + 1].first, 0 }) >= min_d)
+        if (pow(v[mid + 1].first - v[i].first, 2) >= min_d)
             break;
         w.push_back(v[i]);
     }
     for (int i = mid + 1; i <= r; i++) {
-        if (dist({ v[mid].first, 0 }, { v[i].first, 0 }) >= min_d)
+        if (pow(v[i].first - v[mid].first, 2) >= min_d)
             break;
         w.push_back(v[i]);
     }
     sort(w.begin(), w.end(), cmp_y());
-
     for (int i = 0; i < w.size(); i++) {
         for (int j = i + 1; j < w.size(); j++) {
-            if (dist({ 0, w[i].second }, { 0, w[j].second }) >= min_d)
+            if (pow(w[j].second - w[i].second, 2) >= min_d)
                 break;
             min_d = min(min_d, dist(w[i], w[j]));
         }
@@ -55,7 +54,6 @@ int main() {
     v.resize(N);
     for (int i = 0; i < N; i++)
         scanf("%d %d", &v[i].first, &v[i].second);
-    sort(v.begin(), v.end());
-    
+    sort(v.begin(), v.end());  // x축 정렬
     printf("%d", dc(0, N - 1));
 }
