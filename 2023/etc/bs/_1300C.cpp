8727a381d34706a545d7175c63c00f6d374e69ed
canplane <canplane@gmail.com>
2023-05-26 23:10:35 +0900

    reorganized

--- 2023/etc/bs/_1300C.cpp
+++ 2023/etc/bs/_1300C.cpp
@@ -2,31 +2,31 @@ using namespace std;
 #include <cstdio>
 #include <algorithm>
 
-long get_idx(long N, long val)
+long val_to_idx(long val, long N)
 {
 	long idx = 0;
-	for (int i = 1; i <= N; i++) {
+	for (int i = 1; i <= N; i++)
 		idx += min(val / i, N);
-	}
 	return idx;
 }
-int bs(long N, long lo, long hi, long k)
+int inv_lower_bound(long val_l, long val_r, long idx, long N)	// [l, r)
 {
-	if (lo == hi) {
-		return lo;
+	while (val_l < val_r) {
+		long val_m = (val_l + val_r) / 2;
+		long idx_m = val_to_idx(val_m, N);
+		// lower bound: stretches to the minimum of values that can have same index.
+		if (idx <= idx_m)
+			val_r = val_m;
+		else
+			val_l = val_m + 1;
 	}
-	long mi = (lo + hi) / 2;
-	long idx = get_idx(N, mi);
-	if (k <= idx)	// lower bound
-		return bs(N, lo, mi, k);
-	else
-		return bs(N, mi + 1, hi, k);
+	return val_l;
 }
 
 int main()
 {
 	long N, k;
 	scanf("%ld %ld", &N, &k);
-	long ans = bs(N, 1, N * N, k);
+	long ans = inv_lower_bound(1, N * N + 1, k, N);
 	printf("%ld", ans);
 }
