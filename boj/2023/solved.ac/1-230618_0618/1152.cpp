fc65947ba445a8ad58585838afd1a81ec16405b6
canplane <canplane@gmail.com>
2023-06-23 19:17:45 +0900

    230623

--- boj/2023/solved.ac/1-230618_0618/1152.cpp
+++ boj/2023/solved.ac/1-230618_0618/1152.cpp
@@ -1,31 +1,12 @@
-using namespace std;
 #include <cstdio>
-#include <iostream>
+
+char s[1000001];
 
 int main()
 {
-	string s;
-	getline(cin, s);
-
-	// strip
-	if (s.find(" ", 0) == 0) {
-		s.replace(0, 1, "");
-	}
-	// rstrip
-	if (s.length() > 0 && s.rfind(" ", string::npos) == s.length() - 1) {
-		s.replace(s.length() - 1, 1, "");
-	}
-	
-	// split
-	if (s.empty()) {
-		cout << 0;
-	}
-	else {
-		int cnt = 1;
-		int i = -1;
-		while ((i = s.find(" ", i + 1)) != string::npos) {
-			cnt += 1;
-		}
-		cout << cnt;
-	}
+    int ans = 0;
+    while (scanf("%s", s) == 1) {
+        ans++;
+    }
+    printf("%d", ans);
 }
