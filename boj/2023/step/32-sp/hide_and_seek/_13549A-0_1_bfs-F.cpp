80ea54245bc7e2c5f675d04e36c89c462bd780d4
canplane <canplane@gmail.com>
2023-07-13 00:36:42 +0900

    to BOJ step 32 (Shortest path)

--- boj/2023/step/32-sp/hide_and_seek/_13549A-0_1_bfs-F.cpp
+++ boj/2023/step/32-sp/hide_and_seek/_13549A-0_1_bfs-F.cpp
@@ -2,8 +2,9 @@
 // https://www.acmicpc.net/board/view/121219
 // 0-1 BFS 기본 문제. (O(V + E) 0-1 BFS가 O((V + E)logV) 다익스트라보다 효율적이므로)
 // 0-1 BFS는 매 시점 후보들의 값 차이가 at most 1로, 중간에 값을 끼워넣을 필요가 없어 덱을 쓰는 것이지, 기본적으로는 우선순위 큐 기반 다익스트라와 로직이 동일.
-// 따라서 다익스트라와 동일하게 실제 방문 시 visit을 set해야 하는 것에 주의해야 함. (이것 때문에 틀림)
+// 따라서 다익스트라와 동일하게 실제 방문 시 visit을 set해야 하는 것에 주의해야 함.
 // 반례: 4 6 (4 -> 5 -> 6이 아니라 4 -> 3 -> 6)
+// 최단 경로 알고리즘 설명: https://anz1217.tistory.com/23
 
 using namespace std;
 #include <cstdio>
@@ -26,6 +27,7 @@ int main()
 			break;
 		}
 
+		// 0-1 BFS는 visit을 push 시 set하면 안 됨! (다익스트라가 그러한 것처럼)
 		int v;
 		if ((v = 2 * u) <= 200000 && !visit[v]) {
 			visit[v] = true, q.push_front({ v, d });
